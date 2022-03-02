d378efb0036fb65c86837c9ef6000a6d9dbb4bb0
DbomDev <72315013+DbomDev@users.noreply.github.com>
2022-03-02 05:40:49 -0800

    Update serial-generator.py

--- serial-generator.py
+++ serial-generator.py
@@ -5,7 +5,7 @@ serial_lengh = 30
 serial_amout = 1000
 
 start_text = ""
-chars = 'abcdefghijklmnopqrstuvwxyz'
+chars = 'abcdefghijklmnopqrstuvwxyz123456789_-'
 
 
 # ~~~~~~~~ Do Not Touch anything udner this line! ~~~~~~~~
